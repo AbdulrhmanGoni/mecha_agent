84f184138fee09c83b06847f2c412ff6ec20e016
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-12-11 14:54:08 +0300

    Create `EmbeddingService` class
    `EmbeddingService` is a class provides one method
    takes a string and embeds it into a vector using `ollama`
    platform and an embedding model like, E.g "all-minilm".

---
+++ src/services/EmbeddingService.ts
@@ -0,0 +1,17 @@
+import { Ollama } from "npm:ollama";
+import parsedEnvVariables from "../configurations/parseEnvironmentVariables.ts";
+
+export class EmbeddingService {
+    constructor(private readonly ollamaClient: Ollama) { }
+
+    async embedText(text: string) {
+        return await this.ollamaClient.embeddings({
+            keep_alive: "1h",
+            model: parsedEnvVariables.EMBEDDING_MODEL_NAME,
+            prompt: text,
+            options: {
+                embedding_only: true,
+            }
+        });
+    }
+}
