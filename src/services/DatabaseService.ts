753d6ec0f823c416b5b2632475e0d0549ca08dd0
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-12-24 18:57:30 +0300

    Create "agents" table schema
    Create `createAgentsTable` method in `DatabaseService`
    class which is responsible for defining the schema of
    "agents" table and creating the table (if not exists) while
    bootstrapping the application.

--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -5,6 +5,7 @@ export class DatabaseService {
 
     async init() {
         await this.dbClient.queryObject`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`;
+        await this.createAgentsTable();
     }
 
     private async checkTableExistance(tableName: string) {
@@ -16,6 +17,26 @@ export class DatabaseService {
         `, [tableName]
         );
         return result.rows[0].exists
+    }
+
+    private async createAgentsTable() {
+        const tableExists = await this.checkTableExistance("agents");
+
+        if (!tableExists) {
+            await this.dbClient.queryObject`
+                CREATE TABLE agents (
+                    id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+                    agent_name VARCHAR(80) NOT NULL,
+                    description TEXT NOT NULL,
+                    avatar VARCHAR(42),
+                    system_instructions TEXT,
+                    dataset_id VARCHAR(50),
+                    dont_know_response TEXT,
+                    response_syntax VARCHAR(10),
+                    created_at TIMESTAMP NOT NULL DEFAULT now()
+                )
+            `;
+        }
     }
 
     get query() {
