849b057bd6383e37973c6f796a5fe13bbe993090
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-10 17:23:58 +0300

    Create `create` method in `AgentsService` class
    Create `create` method in `AgentsService` class and
    write the logic of creating new agents in it.

--- src/services/AgentsService.ts
+++ src/services/AgentsService.ts
@@ -17,4 +17,62 @@ export class AgentsService {
         private objectStorageService: ObjectStorageService,
     ) { }
 
+    async create(newAgent: CreateAgentFormData) {
+        const { avatar, ...agentData } = newAgent
+
+        let avatarId = null;
+
+        if (avatar) {
+            const fileExtention = mimeTypeToFileExtentionMap[avatar.type];
+            avatarId = `${crypto.randomUUID()}.${fileExtention}`;
+        }
+
+        type CreationDataFormat = [string, string, string[]];
+
+        const [fields, placeholders, values] = Object
+            .entries({ ...agentData, avatar: avatarId })
+            .reduce<CreationDataFormat>(([fields, placeholders, values], [field, value], i, arr) => {
+                return [
+                    (
+                        fields +
+                        `${agentRowFieldsNamesMap[field] || field}` +
+                        `${i === arr.length - 1 ? "" : ", "}`
+                    ),
+                    (
+                        `${placeholders}$${i + 1}` +
+                        `${i === arr.length - 1 ? "" : ", "}`
+                    ),
+                    [...values, value || ""]
+                ]
+            }, ["", "", []])
+
+        const transaction = this.databaseService.createTransaction("creating_agent")
+        await transaction.begin();
+
+        const result = await transaction.queryObject(`
+            INSERT INTO agents (${fields})
+            VALUES (${placeholders});
+        `, values)
+
+        if (result.rowCount) {
+            if (newAgent.avatar && avatarId) {
+                try {
+                    await this.objectStorageService.uploadFile(
+                        this.objectStorageService.buckets.agentsAvatars,
+                        newAgent.avatar,
+                        { id: avatarId }
+                    );
+                } catch {
+                    await transaction.rollback();
+                    return false;
+                }
+            }
+
+            await transaction.commit();
+            return true;
+        }
+
+        await transaction.rollback();
+        return false;
+    }
 }
