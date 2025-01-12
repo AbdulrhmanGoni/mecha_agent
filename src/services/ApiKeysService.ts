b52ff9aa86a07e8b62eca8b7d39cb0748a11e3c2
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-12 23:52:51 +0300

    Create `getAll` & `getOne` methods in `ApiKeysService` class

--- src/services/ApiKeysService.ts
+++ src/services/ApiKeysService.ts
@@ -23,4 +23,21 @@ export class ApiKeysService {
         return result.rows[0]
     }
 
+    async getAll() {
+        const result = await this.databaseService.query<ApiKeyRecord>({
+            text: "SELECT * FROM api_keys",
+            camelCase: true
+        })
+        return result.rows
+    }
+
+    async getOne(id: string[]) {
+        const result = await this.databaseService.query<ApiKeyRecord>({
+            text: "SELECT * FROM api_keys WHERE id = $1",
+            args: [id],
+            camelCase: true,
+        })
+
+        return result.rows[0]
+    }
 }
