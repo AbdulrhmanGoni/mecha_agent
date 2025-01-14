1d0d75e1ebd0315167561aff6a23042a2989b473
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-14 23:38:46 +0300

    Create the file where services are bootstrapped
    Create `bootstrapServices` function which receives
    the needed dependencies to bootstrap or initialize
    services classes and injecting the dependencies for
    each services class.

---
+++ src/services/bootstrap.ts
@@ -0,0 +1,55 @@
+import { Ollama } from "npm:ollama";
+import { QdrantClient } from "npm:@qdrant/js-client-rest";
+import { Client as MinioClient } from "minio";
+import { EmbeddingService } from "./EmbeddingService.ts";
+import { VectorDatabaseService } from "./VectorDatabaseService.ts";
+import { InstructionsService } from "./InstructionsService.ts";
+import { LLMService } from "./LLMService.ts";
+import { Client as PostgresClient } from "deno.land/x/postgres";
+import { DatabaseService } from "./DatabaseService.ts";
+import { ApiKeysService } from "./ApiKeysService.ts";
+import { JwtService } from "./JwtService.ts";
+import { GuardService } from "./GuardService.ts";
+import { AgentsService } from "./AgentsService.ts";
+import { ObjectStorageService } from "./ObjectStorageService.ts";
+
+type ServicesDependencies = {
+    vectorDatabaseClient: QdrantClient;
+    databaseClient: PostgresClient;
+    ollamaClient: Ollama;
+    minioClient: MinioClient;
+}
+
+export async function bootstrapServices(dependencies: ServicesDependencies) {
+    const embeddingService = new EmbeddingService(dependencies.ollamaClient);
+
+    const vectorDatabaseService = new VectorDatabaseService(dependencies.vectorDatabaseClient, embeddingService);
+    await vectorDatabaseService.init();
+
+    const databaseService = new DatabaseService(dependencies.databaseClient);
+    await databaseService.init();
+
+    const objectStorageService = new ObjectStorageService(dependencies.minioClient);
+    await objectStorageService.init();
+
+    const instructionsService = new InstructionsService(vectorDatabaseService);
+
+    const llmService = new LLMService(dependencies.ollamaClient);
+
+    const jwtService = new JwtService();
+    await jwtService.init();
+
+    const agentsService = new AgentsService(databaseService, objectStorageService);
+
+    const apiKeysService = new ApiKeysService(databaseService, jwtService);
+
+    const guardService = new GuardService(jwtService);
+
+    return {
+        instructionsService,
+        agentsService,
+        apiKeysService,
+        objectStorageService,
+        guardService,
+    }
+};
