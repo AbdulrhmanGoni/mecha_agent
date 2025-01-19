733b634f1b78e1fb46471625c6f85a4a027d722c
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-19 14:43:41 +0300

    Declare datasets types & refactor instructions types
    Create `Dataset` and `CreateDatasetInput` types
    
    Refactor `Instruction` type and split it into two types
    which are `BaseInstruction` and `Instruction` types.

--- src/helpers/embedInstructionFormat.ts
+++ src/helpers/embedInstructionFormat.ts
@@ -1,4 +1,4 @@
-export default function embedInstructionFormat(instruction: Omit<Instruction | UpdateInstructionInput, "id" | "datasetId">) {
+export default function embedInstructionFormat(instruction: Omit<BaseInstruction | UpdateInstructionInput, "id" | "datasetId">) {
     const instructionText = (
         `${instruction.systemMessage ? `"${instruction.systemMessage}"` : ""}` +
         `"${instruction.prompt}" ` +
--- types/instructions.d.ts
+++ types/instructions.d.ts
@@ -1,9 +1,25 @@
-type Instruction = {
+type Dataset = {
     id: string;
-    datasetId: string;
+    agentId: string;
+    title: string;
+    description: string;
+    status: "processing" | "processed" | "unprocessed";
+    createdAt: string;
+}
+
+type CreateDatasetInput = Pick<Dataset, "title" | "description" | "agentId"> & {
+    datasetFile: File;
+}
+
+type BaseInstruction = {
     systemMessage?: string;
     prompt: string;
     response: string;
+}
+
+type Instruction = BaseInstruction & {
+    id: string;
+    datasetId: string;
 }
 
 type InstructionInput = Instruction;
