eac539524479bef9b0aa3e41292fb4ed1360aa58
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-21 17:37:35 +0300

    Create `systemMessageTemplete` helper function
    This function is used to create the system message of
    an agent to be passed then to the LLM

---
+++ src/helpers/systemMessageTemplete.ts
@@ -0,0 +1,22 @@
+import parsedEnvVariables from "../configurations/parseEnvironmentVariables.ts";
+import { responseSyntaxesPrompt } from "../constant/responseSyntaxes.ts";
+
+export default function systemMessageTemplete({
+    agentName,
+    description,
+    systemInstructions,
+    dontKnowResponse,
+    responseSyntax
+}: Agent) {
+
+    const fallbackResponse = dontKnowResponse || parsedEnvVariables.DEFAULT_DONT_KNOW_RESPONSE;
+    const syntax = responseSyntaxesPrompt[responseSyntax] || "";
+    const additionalInstructions = systemInstructions ? ", " + systemInstructions : "";
+
+    return (
+        `You are '${agentName}', and your description is "${description}",` +
+        "Your job is to respond to the users based on the provided context, " +
+        `If you couldn't find a suitable answer from the context, ` +
+        `Just respond '${fallbackResponse}', ${syntax}` + additionalInstructions
+    )
+};
