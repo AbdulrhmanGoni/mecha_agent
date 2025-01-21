fbe2667de0cf30bba6485b00e5bd7ac198507a34
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-22 00:09:43 +0300

    Refactor permissions file

--- src/constant/permissions.ts
+++ src/constant/permissions.ts
@@ -1,5 +1,11 @@
-const permissions = ["inference", "read", "write"] as const
-
 export type Permission = "inference" | "read" | "write";
 
-export default permissions
+export const inferencePermission = "inference"
+export const readPermission = "read"
+export const writePermission = "write"
+
+export const permissionsArray = [
+    inferencePermission,
+    readPermission,
+    writePermission
+] as const
