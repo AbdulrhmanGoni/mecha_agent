1467e1d87c3b3c5205b34416e3292709558b4b19
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-23 22:16:31 +0300

    Create `fetchInstructions` method in `ChatsService` class
    `fetchInstructions` is the responsible method for fetching
    the instructions of the agent by taking user's prompt and
    searching for similar instructions inside the vector store
    and returning them.

--- src/services/ChatsService.ts
+++ src/services/ChatsService.ts
@@ -9,4 +9,29 @@ export class ChatsService {
         private readonly llmService: LLMService,
     ) { }
 
+    async fetchInstructions({ agentId, prompt }: Pick<ChatRelatedTypes, "prompt" | "agentId">) {
+        const { rows: [agentRow] } = await this.databaseService.query<Agent>({
+            text: `SELECT * FROM agents WHERE id = $1`,
+            args: [agentId],
+            camelCase: true,
+        })
+
+        if (!agentRow.datasetId) {
+            return `Sorry!, I don't have a dataset to answer based on.`
+        }
+
+        const searchResult = await this.vectorDatabaseService.search(
+            agentRow.datasetId,
+            prompt
+        );
+
+        if (!searchResult.length) {
+            return agentRow.dontKnowResponse || parsedEnvVariables.DEFAULT_DONT_KNOW_RESPONSE
+        }
+
+        return {
+            instructions: searchResult,
+            agent: agentRow,
+        }
+    }
 }
