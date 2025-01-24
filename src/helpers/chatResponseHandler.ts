105448d04d011dc1c60efec441e79ab0f94351b7
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-24 14:10:26 +0300

    Create `chatResponseHandler` function
    `chatResponseHandler` function is responsible for
    handling the responses returned from `ollama.chat`
    method (LLMs answer).
    
    The function transforms the response stream into a
    new readable stream (that supposed to be served  to
    the client which sent the chat request via The API),
    
    While transforming the response stream into the
    new readable stream, The function collects the
    response text to be saved in chat's record in the
    database.

---
+++ src/helpers/chatResponseHandler.ts
@@ -0,0 +1,28 @@
+import { encoder } from "djwt/util";
+
+type ChatResponseHandlerParams = {
+    llmResponse: any;
+    onResponseComplete: (text: string) => Promise<void>;
+}
+
+export default function chatResponseHandler({ onResponseComplete, llmResponse }: ChatResponseHandlerParams): ReadableStream {
+    const { readable, writable } = new TransformStream();
+
+    (async () => {
+        const writer = writable.getWriter();
+        let responseText = "";
+
+        for await (const llmResponsePart of llmResponse) {
+            writer.write(encoder.encode(llmResponsePart.message.content));
+            responseText += llmResponsePart.message.content
+        }
+
+        writer.close();
+
+        return responseText
+    })().then(async (responseText) => {
+        await onResponseComplete(responseText)
+    })
+
+    return readable;
+};
