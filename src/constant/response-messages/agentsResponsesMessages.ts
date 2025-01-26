08bf614fd94d947a3b9bdf07e2cdad0df295bd4b
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-27 00:46:19 +0300

    Create `AgentsController` class with 1 method & 1 dependency
    Create `AgentsController` class with one method which is
    `create` method and one dependency which is an instance of
    `AgentsService` class.

---
+++ src/constant/response-messages/agentsResponsesMessages.ts
@@ -0,0 +1,6 @@
+const AgentsResponseMessages = {
+    successfulAgentCreation: "The agent was created successfully",
+    failedAgentCreation: "Failed to create the agent",
+}
+
+export default AgentsResponseMessages
---
+++ src/controllers/AgentsController.ts
@@ -0,0 +1,19 @@
+import { Context } from "npm:hono";
+import { AgentsService } from "../services/AgentsService.ts";
+import AgentsResponseMessages from "../constant/response-messages/agentsResponsesMessages.ts";
+
+export class AgentsController {
+    constructor(private agentsService: AgentsService) { }
+
+    async create(c: Context<never, never, { out: { form: CreateAgentFormData } }>) {
+        const formData = c.req.valid("form");
+        const result = await this.agentsService.create(formData);
+
+        if (result) {
+            return c.json({ result: AgentsResponseMessages.successfulAgentCreation }, 201);
+        }
+
+        return c.json({ error: AgentsResponseMessages.failedAgentCreation }, 404);
+    }
+}
+
