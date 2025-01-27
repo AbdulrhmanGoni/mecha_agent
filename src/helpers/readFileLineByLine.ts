638e3cc5ae85929d164ec9234e278cb15997b839
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-27 18:06:06 +0300

    Create `readFileLineByLine` function
    This function takes a readable stream object (which
    is a dataset file stored the object storage under
    `datasets` backet) as 1st parameter to handle reading
    it line by line and calls `onLine` function option with
    each line to be processed.

---
+++ src/helpers/readFileLineByLine.ts
@@ -0,0 +1,27 @@
+import type { Readable } from "node:stream";
+
+type ReadFileLineByLineOptions = {
+    onLine: (line: string, lineNumber?: number) => void;
+}
+
+export async function readFileLineByLine(readableFile: Readable, { onLine }: ReadFileLineByLineOptions) {
+    const decoder = new TextDecoder();
+    let buffer = "";
+    let lineNumber = 1;
+
+    for await (const chunk of readableFile) {
+        buffer += decoder.decode(chunk, { stream: true });
+
+        const lines = buffer.split("\n");
+        buffer = lines.pop() || "";
+
+        for (const line of lines) {
+            onLine(line, lineNumber)
+            lineNumber++
+        }
+    }
+
+    if (buffer) {
+        onLine(buffer, lineNumber)
+    }
+}
