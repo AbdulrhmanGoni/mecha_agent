6408c966df9e7928838b300d02bb3922327fb519
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-28 17:46:25 +0300

    Add two methods to `InstructionsController` class
    Add `removeInstructions` and `clearInstructions` methods
    to `InstructionsController` class.

--- src/controllers/InstructionsController.ts
+++ src/controllers/InstructionsController.ts
@@ -16,4 +16,19 @@ export class InstructionsController {
         const result = await this.instructionsService.updateInstructions(instructions);
         return c.json({ result });
     }
+
+    async removeInstructions(c: Context<never, never, { out: { json: string[] } }>) {
+        const instructionsIds = c.req.valid("json");
+        const result = await this.instructionsService.removeInstructions(instructionsIds);
+        return c.json({ result });
+    }
+
+    async clearInstructions(c: Context) {
+        const datasetId = c.req.query("datasetId");
+        if (!datasetId) {
+            throw new HTTPException(400, { message: "No dataset id provided to clear its instructions" });
+        }
+        const result = await this.instructionsService.clearInstructions(datasetId);
+        return c.json({ result });
+    }
 }
