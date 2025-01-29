0b01e9d2ab7d80982a8cd7c487a93bf64fe95eb2
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-29 20:30:57 +0300

    Refactor and rephrase system message template

--- src/helpers/systemMessageTemplete.ts
+++ src/helpers/systemMessageTemplete.ts
@@ -10,13 +10,14 @@ export default function systemMessageTemplete({
 }: Agent) {
 
     const fallbackResponse = dontKnowResponse || parsedEnvVariables.DEFAULT_DONT_KNOW_RESPONSE;
-    const syntax = responseSyntaxesPrompt[responseSyntax] || "";
+    const syntaxPrompt = responseSyntaxesPrompt[responseSyntax];
     const additionalInstructions = systemInstructions ? ", " + systemInstructions : "";
 
     return (
         `You are '${agentName}', and your description is "${description}",` +
         "Your job is to respond to the users based on the provided context, " +
         `If you couldn't find a suitable answer from the context, ` +
-        `Just respond '${fallbackResponse}', ${syntax}` + additionalInstructions
+        `Just respond '${fallbackResponse}'${syntaxPrompt ? ", " + syntaxPrompt : ""}` +
+        additionalInstructions
     )
 };
