47c3664f7fd5ed96ea8e1ef3c2857681fea8c1f9
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-30 17:48:52 +0300

    Adjust `temperature` parameter of the custom model
    Set `temperature` parameter of the custom model when
    creating it in `scripts/createCustomModel.ts` file to '0.1'
    for making the model less creative to avoid the innovative
    answers outside the context.
    
    And also adjust the log messages in the file.

--- scripts/createCustomModel.ts
+++ scripts/createCustomModel.ts
@@ -3,7 +3,11 @@ import parsedEnvVariables from "../src/configurations/parseEnvironmentVariables.
 
 const ollamaClient = new Ollama({ host: parsedEnvVariables.OLLAMA_HOST });
 
-const modelfile = `FROM ${parsedEnvVariables.BASE_MODEL_NAME}`
+const modelfile = `
+FROM ${parsedEnvVariables.BASE_MODEL_NAME}
+
+PARAMETER temperature 0.1
+`
 
 const response = await ollamaClient.create({
     model: parsedEnvVariables.MODEL_NAME,
@@ -12,6 +16,6 @@ const response = await ollamaClient.create({
 
 console.log(
     response.status === "success" ?
-        `Model '${parsedEnvVariables.MODEL_NAME}' created successfully` :
-        `Model '${parsedEnvVariables.MODEL_NAME}' creation failed`
+        `Model '${parsedEnvVariables.MODEL_NAME}' created based on ${parsedEnvVariables.BASE_MODEL_NAME} model successfully` :
+        `Model '${parsedEnvVariables.MODEL_NAME}' creation based on ${parsedEnvVariables.BASE_MODEL_NAME} model failed`
 )
