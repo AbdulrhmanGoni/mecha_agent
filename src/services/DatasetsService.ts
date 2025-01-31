f35484b7f86905f20b0834caa0a4a76776f5a0c5
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-01-31 13:52:49 +0300

    Create `create` method in `DatasetsService` class
    `create` method which is responsible to create a dataset
    for an agent.
    
    The function creates dataset row in datasets table, sets the
    `dataset_id` field of the agent to the id of the created dataset yet,
    Uploads the dataset file onto datasets bucket on the object storage,
    And lastly posts a message to dataset processing worker to process
    the dataset.

--- src/services/DatasetsService.ts
+++ src/services/DatasetsService.ts
@@ -10,4 +10,47 @@ export class DatasetsService {
         private readonly sseService: SSEService,
     ) {
     }
+
+    async create(datasetInput: CreateDatasetInput) {
+        const newDatasetId = crypto.randomUUID();
+
+        const transactionSession = this.databaseService.createTransaction("dataset_creation");
+        await transactionSession.begin();
+
+        const { rows: [dataset] } = await transactionSession.queryObject<Dataset>({
+            text: `INSERT INTO datasets (id, agent_id, title, description, status) VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
+            args: [newDatasetId, datasetInput.agentId, datasetInput.title, datasetInput.description, "processing"],
+            camelCase: true
+        });
+
+        const { rowCount: updatedAgents } = await transactionSession.queryObject<Dataset>({
+            text: "UPDATE agents SET dataset_id = $2 WHERE id = $1",
+            args: [dataset.agentId, dataset.id],
+            camelCase: true
+        });
+
+        if (dataset && updatedAgents === 1) {
+            try {
+                await this.objectStorageService.uploadFile(
+                    this.objectStorageService.buckets.datasets,
+                    datasetInput.datasetFile,
+                    { id: dataset.id }
+                );
+
+                await transactionSession.commit();
+
+                this.datasetProcessingWorker.postMessage({
+                    process: "new_dataset",
+                    payload: dataset,
+                });
+
+                return dataset;
+            } catch {
+                //
+            }
+        }
+
+        await transactionSession.rollback();
+        return null;
+    }
 }
