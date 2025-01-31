23a3339b95df53959cf0b3ddfd7acf83fbfffa77
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-01 01:00:47 +0300

    Create `ChatsController` class with two methods
    Create `ChatsController` class with `startChat` and
    `continueChat` methods to handle chats requests.

---
+++ src/controllers/ChatsController.ts
@@ -0,0 +1,34 @@
+import { Context } from "npm:hono";
+import { ChatsService } from "../services/ChatsService.ts";
+
+export class ChatsController {
+    constructor(private chatsService: ChatsService) { }
+
+    async startChat(c: Context) {
+        const agentId = c.req.query("agentId") as string;
+        const body = await c.req.json();
+
+        const { chatResponse, chatId } = await this.chatsService.startChat({
+            agentId,
+            prompt: body.prompt,
+            user: c.get("user")
+        });
+
+        return c.body(chatResponse, 200, { chatId });
+    }
+
+    async continueChat(c: Context) {
+        const chatId = c.req.param("chatId");
+        const agentId = c.req.query("agentId") as string;
+        const body = await c.req.json();
+
+        const response = await this.chatsService.continueChat({
+            agentId,
+            prompt: body.prompt,
+            chatId,
+            user: c.get("user")
+        });
+
+        return c.body(response, 200);
+    }
+}
