15a2755d3b3a711ba632975db6547143bbe8e7af
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-01 14:24:12 +0300

    Create `bootstrapControllers` function
    The job of `bootstrapControllers` function is similar to
    the concept of "Dependency Injection Container", It initializes
    all controllers that handle the API requests, It takes the
    services that the controllers depend on as parameter and
    injects into each controller the service that it depends on.

---
+++ src/controllers/bootstrap.ts
@@ -0,0 +1,73 @@
+import { AgentsController } from "./AgentsController.ts";
+import { ApiKeysController } from "./ApiKeysController.ts";
+import { AuthController } from "./AuthController.ts";
+import { ChatsController } from "./ChatsController.ts"
+import { InstructionsController } from "./InstructionsController.ts";
+import { MediaController } from "./MediaController.ts";
+import { DatasetsController } from "./DatasetsController.ts";
+import { SSEController } from "./SSEController.ts";
+import { AgentsService } from "../services/AgentsService.ts";
+import { ApiKeysService } from "../services/ApiKeysService.ts";
+import { AuthService } from "../services/AuthService.ts";
+import { ChatsService } from "../services/ChatsService.ts";
+import { InstructionsService } from "../services/InstructionsService.ts";
+import { ObjectStorageService } from "../services/ObjectStorageService.ts";
+import { SSEService } from "../services/SSEService.ts";
+import { DatasetsService } from "../services/DatasetsService.ts";
+
+type controllersDependencies = {
+    instructionsService: InstructionsService;
+    agentsService: AgentsService;
+    datasetsService: DatasetsService;
+    authService: AuthService;
+    apiKeysService: ApiKeysService;
+    chatsService: ChatsService;
+    objectStorageService: ObjectStorageService;
+    sseService: SSEService;
+}
+
+export default function bootstrapControllers(dependencies: controllersDependencies) {
+    const instructionsController = new InstructionsController(
+        dependencies.instructionsService
+    );
+
+    const agentsController = new AgentsController(
+        dependencies.agentsService
+    );
+
+    const datasetsController = new DatasetsController(
+        dependencies.datasetsService
+    );
+
+    const chatsController = new ChatsController(
+        dependencies.chatsService
+    );
+
+    const mediaController = new MediaController(
+        dependencies.objectStorageService
+    );
+
+    const apiKeysController = new ApiKeysController(
+        dependencies.apiKeysService
+    );
+
+    const authController = new AuthController(
+        dependencies.authService
+    );
+
+    const sseController = new SSEController(
+        dependencies.sseService
+    );
+
+    return {
+        chatsController,
+        instructionsController,
+        agentsController,
+        datasetsController,
+        mediaController,
+        apiKeysController,
+        authController,
+        sseController,
+    }
+};
+
