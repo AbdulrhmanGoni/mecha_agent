d700ba8521cb85805bde69676be966c01bd1ad45
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-04 01:01:24 +0300

    Create `createApiKeyInputValidator` middleware

---
+++ src/validation/apiKeys/createApiKeyInputValidator.ts
@@ -0,0 +1,20 @@
+import { z } from 'npm:zod'
+import { validator } from 'npm:hono/validator'
+import { permissionsArray } from "../../constant/permissions.ts";
+import schemaParser from "../../helpers/schemaParser.ts";
+
+const createApiKeyInputSchema = z.object({
+    keyName: z.string(),
+    permissions: z.array(z.enum(permissionsArray)).nonempty("Specify at least one permission"),
+    maxAgeInDays: z.number(),
+}).strict()
+
+const createApiKeyInputValidator = validator('json', (value, c) => {
+    return schemaParser<typeof createApiKeyInputSchema.shape>(
+        c,
+        createApiKeyInputSchema,
+        value
+    );
+})
+
+export default createApiKeyInputValidator;
