e572b866e4fff3e798b5b70d2835944747a8cbee
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-04 01:14:57 +0300

    Create dataset schema
    Create the schema that all datasets inputs will
    be validated based on it.

---
+++ src/validation/datasets/datasetSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "npm:zod@3";
+import { supportedDatasetFileTypes } from "../../constant/supportedFileTypes.ts";
+import agentIdValidator from "../agents/agentIdValidator.ts";
+
+const datasetFileValidator = z
+    .instanceof(File)
+    .refine((file) => file.size && file.name, { message: "Invalid file" })
+    .refine(
+        (file) => supportedDatasetFileTypes.some((extention) => file.type.endsWith(extention)),
+        (file) => ({
+            message: `Not supported file type '${file.type}', The supported types are: ${supportedDatasetFileTypes.join(', ')}`
+        })
+    )
+
+const datasetSchema = z.object({
+    title: z.string().max(70),
+    description: z.string().max(150),
+    agentId: agentIdValidator,
+    datasetFile: datasetFileValidator,
+})
+
+export default datasetSchema
