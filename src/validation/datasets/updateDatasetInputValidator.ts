5db2c3ce87e3f72f0505e53f321429fb297a4276
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-04 01:15:42 +0300

    Create `updateDatasetInputValidator` middleware

---
+++ src/validation/datasets/updateDatasetInputValidator.ts
@@ -0,0 +1,20 @@
+import { z } from 'npm:zod'
+import { validator } from 'npm:hono/validator'
+import schemaParser from "../../helpers/schemaParser.ts";
+import { ParsedFormValue } from "npm:hono/types";
+import datasetSchema from "./datasetSchema.ts";
+
+export const updateDatasetInputSchema = z.object({
+    title: datasetSchema.shape.title.optional(),
+    description: datasetSchema.shape.description.optional(),
+}).strict();
+
+const updateDatasetInputValidator = validator('json', (value, c) => {
+    return schemaParser<typeof updateDatasetInputSchema.shape, ParsedFormValue | ParsedFormValue[]>(
+        c,
+        updateDatasetInputSchema,
+        value
+    );
+})
+
+export default updateDatasetInputValidator;
