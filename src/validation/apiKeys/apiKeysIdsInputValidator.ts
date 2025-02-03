2b5112cc20311a4744daf96095f4684b9346df37
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-04 01:00:37 +0300

    Create `apiKeysIdsInputValidator` middleware

---
+++ src/validation/apiKeys/apiKeysIdsInputValidator.ts
@@ -0,0 +1,22 @@
+import { z } from 'npm:zod';
+import { validator } from 'npm:hono/validator';
+import schemaParser from "../../helpers/schemaParser.ts";
+
+const apiKeyIdValidator = z.string().uuid({
+    message: "API Key id must be UUID"
+})
+
+const apiKeysIdsInputValidator = validator('json', (value, c) => {
+    const apiKeysIdsInputSchema = z
+        .array(apiKeyIdValidator)
+        .nonempty("No API Keys ids provided");
+
+    return schemaParser(
+        c,
+        apiKeysIdsInputSchema,
+        value,
+        (message, issues) => `${message} ${issues.message} at: index ${issues.path}\n`
+    )
+})
+
+export default apiKeysIdsInputValidator;
