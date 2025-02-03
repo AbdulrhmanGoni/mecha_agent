796c17da88fc8f9ef37d50286a13a57214ab04c6
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-04 00:20:24 +0300

    Create `removeInstructionsInputValidator` middleware

---
+++ src/validation/instructions/removeInstructionsInputValidator.ts
@@ -0,0 +1,19 @@
+import { z } from 'npm:zod';
+import { validator } from 'npm:hono/validator';
+import schemaParser from "../../helpers/schemaParser.ts";
+
+const instructionIdValidator = z.string({ message: "instruction id must be string" });
+
+const removeInstructionsInputValidator = validator('json', (value, c) => {
+    const removeInstructionsInputSchema = z
+        .array(instructionIdValidator)
+        .nonempty("Specify at least one instruction to delete");
+
+    return schemaParser(
+        c,
+        removeInstructionsInputSchema,
+        value,
+        (message, issues) => `${message} ${issues.message} at: index ${issues.path}\n`
+    )
+})
+export default removeInstructionsInputValidator;
