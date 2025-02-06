2d60265555b8b05db9efcbb1fbc4691b2c80a0f2
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-06 14:25:53 +0300

    Create `getChatMessagesInputValidator` middleware

---
+++ src/validation/chats/getChatMessagesInputValidator.ts
@@ -0,0 +1,28 @@
+import { z } from 'npm:zod';
+import { Context } from "npm:hono";
+import schemaParser from "../../helpers/schemaParser.ts";
+import { Next } from "npm:hono/types";
+import agentIdValidator from "../agents/agentIdValidator.ts";
+import chatIdValidator from "./chatIdValidator.ts";
+
+const getChatMessagesInputSchema = z.object({
+    agentId: agentIdValidator,
+    chatId: chatIdValidator,
+}).strict();
+
+export default async function getChatMessagesInputValidator(c: Context, next: Next) {
+    const chatId = c.req.param("chatId");
+    const agentId = c.req.query("agentId");
+
+    const result = schemaParser<typeof getChatMessagesInputSchema.shape, string | undefined>(
+        c,
+        getChatMessagesInputSchema,
+        { agentId, chatId }
+    )
+
+    if (result instanceof Response) {
+        return result
+    }
+
+    await next()
+};
