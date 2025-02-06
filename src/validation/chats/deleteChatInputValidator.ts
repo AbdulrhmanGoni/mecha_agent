efda6d1001e01f29e493a044ff902e06d44a6914
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-06 14:26:17 +0300

    Create `deleteChatInputValidator` middleware

---
+++ src/validation/chats/deleteChatInputValidator.ts
@@ -0,0 +1,28 @@
+import { z } from 'npm:zod';
+import { Context } from "npm:hono";
+import schemaParser from "../../helpers/schemaParser.ts";
+import { Next } from "npm:hono/types";
+import agentIdValidator from "../agents/agentIdValidator.ts";
+import chatIdValidator from "./chatIdValidator.ts";
+
+const deleteChatInputSchema = z.object({
+    agentId: agentIdValidator,
+    chatId: chatIdValidator,
+}).strict();
+
+export default async function deleteChatInputValidator(c: Context, next: Next) {
+    const chatId = c.req.param("chatId");
+    const agentId = c.req.query("agentId");
+
+    const result = schemaParser<typeof deleteChatInputSchema.shape, string | undefined>(
+        c,
+        deleteChatInputSchema,
+        { agentId, chatId }
+    )
+
+    if (result instanceof Response) {
+        return result
+    }
+
+    await next()
+};
