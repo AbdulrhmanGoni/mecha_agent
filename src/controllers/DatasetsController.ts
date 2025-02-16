bed0ddec6b2a356941d5fbbccbded265b2fc6dd7
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-16 14:21:56 +0300

    Update `DatasetsController` class to handle user email

--- src/controllers/DatasetsController.ts
+++ src/controllers/DatasetsController.ts
@@ -4,24 +4,29 @@ import { DatasetsService } from "../services/DatasetsService.ts";
 export class DatasetsController {
     constructor(private datasetsService: DatasetsService) { }
 
-    async create(c: Context<never, never, { out: { form: CreateDatasetInput } }>) {
+    async create(c: Context<{ Variables: { userEmail: string } }, never, { out: { form: CreateDatasetInput } }>) {
         const dataset = c.req.valid("form");
-        const result = await this.datasetsService.create(dataset);
+        const userEmail = c.get("userEmail");
+        const result = await this.datasetsService.create(userEmail, dataset);
 
         return c.json({ result }, 201);
     }
 
-    async delete(c: Context) {
+    async delete(c: Context<{ Variables: { userEmail: string } }>) {
         const datasetId = c.req.param("datasetId");
         const agentId = c.req.query("agentId") as string;
-        const result = await this.datasetsService.delete(agentId, datasetId);
+        const userEmail = c.get("userEmail");
+        const result = await this.datasetsService.delete({ agentId, datasetId, userEmail });
+
         return c.json({ result });
     }
 
-    async update(c: Context<never, never, { out: { json: UpdateDatasetInput } }>) {
+    async update(c: Context<{ Variables: { userEmail: string } }, never, { out: { json: UpdateDatasetInput } }>) {
         const updateData = c.req.valid("json");
         const datasetId = c.req.param("datasetId") as string;
-        const result = await this.datasetsService.update(datasetId, updateData);
+        const userEmail = c.get("userEmail");
+        const result = await this.datasetsService.update(datasetId, userEmail, updateData);
+
         return c.json({ result });
     }
 }
