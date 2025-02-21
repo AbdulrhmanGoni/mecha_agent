f72d226a7c89a4c84c3a960bb2eab738f4cc0dd6
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-02-21 17:47:02 +0300

    Create `bootstrapApp` function (DI Container)
    `bootstrapApp` function is the container where the
    services, controllers and API routes are injected with
    their dependencies to be ready to use,  After API routes
    take their dependencies, they create the app (which is
    an instance of `Hono` class) that is the entrypoint for all
    requests comming into this RAG system.

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { bootstrapServices } from "./services/bootstrap.ts";
+import bootstrapControllers from "./controllers/bootstrap.ts";
+import bootstrapRoutes from "./routes/bootstrap.ts";
+import { bootstrapConfigurations } from "./configurations/bootstrap.ts";
+
+export default async function bootstrapApp() {
+    const configurations = await bootstrapConfigurations();
+
+    const services = await bootstrapServices(configurations);
+
+    const controllers = bootstrapControllers(services);
+
+    const app = bootstrapRoutes({
+        ...controllers,
+        guardService: services.guardService,
+    });
+
+    return app
+};
