440249284d39e4c0cb2dc043565ddfecfb4920b6
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-08 02:45:17 +0300

    Return feedback messages instead of booleans in `DatasetsController`

---
+++ src/constant/response-messages/datasetsResponsesMessages.ts
@@ -0,0 +1,10 @@
+const datasetsResponsesMessages = {
+    successfulUpdate: "The dataset was updated successfully",
+    successfulDeletion: "The dataset was deleted successfully",
+
+    failedUpdate: "Update the dataset failed",
+    failedDeletion: "Dataset deletion failed",
+    failedCreation: "Dataset creation failed",
+}
+
+export default datasetsResponsesMessages
--- src/controllers/DatasetsController.ts
+++ src/controllers/DatasetsController.ts
@@ -1,5 +1,6 @@
 import { Context } from "hono";
 import { DatasetsService } from "../services/DatasetsService.ts";
+import datasetsResponsesMessages from "../constant/response-messages/datasetsResponsesMessages.ts";
 
 export class DatasetsController {
     constructor(private datasetsService: DatasetsService) { }
@@ -9,7 +10,11 @@ export class DatasetsController {
         const userEmail = c.get("userEmail");
         const result = await this.datasetsService.create(userEmail, dataset);
 
-        return c.json({ result }, 201);
+        if (result) {
+            return c.json({ result }, 201);
+        }
+
+        return c.json({ error: datasetsResponsesMessages.failedCreation });
     }
 
     async delete(c: Context<{ Variables: { userEmail: string } }>) {
@@ -18,7 +23,11 @@ export class DatasetsController {
         const userEmail = c.get("userEmail");
         const result = await this.datasetsService.delete({ agentId, datasetId, userEmail });
 
-        return c.json({ result });
+        if (result) {
+            return c.json({ result: datasetsResponsesMessages.successfulDeletion });
+        }
+
+        return c.json({ error: datasetsResponsesMessages.failedDeletion });
     }
 
     async update(c: Context<{ Variables: { userEmail: string } }, never, { out: { json: UpdateDatasetInput } }>) {
@@ -27,7 +36,11 @@ export class DatasetsController {
         const userEmail = c.get("userEmail");
         const result = await this.datasetsService.update(datasetId, userEmail, updateData);
 
-        return c.json({ result });
+        if (result) {
+            return c.json({ result: datasetsResponsesMessages.successfulUpdate });
+        }
+
+        return c.json({ error: datasetsResponsesMessages.failedUpdate });
     }
 }
 
