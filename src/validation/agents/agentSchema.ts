13518764bab65a42a254a411885822030231d7f5
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-08 14:02:43 +0300

    Move `avatarFieldValidator` into a separate file for reusing

--- src/validation/agents/agentSchema.ts
+++ src/validation/agents/agentSchema.ts
@@ -1,22 +1,11 @@
 import { z } from "zod";
 import { responseSyntaxes } from "../../constant/responseSyntaxes.ts";
-import { supportedImageTypes } from "../../constant/supportedFileTypes.ts";
-
-const avatarFieldValidator = z
-    .instanceof(File)
-    .refine((file) => file.size && file.name, { message: "Invalid file" })
-    .refine(
-        (file) => supportedImageTypes.some((extention) => file.type.endsWith(extention)),
-        (file) => ({
-            message: `Not supported file type (${file.type}), Please use images with ${supportedImageTypes.join(', ')} extentions`
-        })
-    )
-    .optional()
+import avatarFieldValidator from "../shared/avatarFieldValidator.ts";
 
 const agentSchema = z.object({
     agentName: z.string().max(40),
     description: z.string().max(100),
-    avatar: avatarFieldValidator,
+    avatar: avatarFieldValidator.optional(),
     systemInstructions: z.string().max(10000).optional(),
     dontKnowResponse: z.string().max(100).optional(),
     greetingMessage: z.string().max(80).optional(),
