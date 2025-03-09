ac92f8997bd5c61ac54b211104dcdbf613e6db0a
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-09 14:14:32 +0300

    Add `update` method to users controller to handle update requests

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,6 +1,7 @@
 import { Context } from "hono";
 import { UsersService } from "../services/UsersService.ts";
 import authResponsesMessages from "../constant/response-messages/authResponsesMessages.ts";
+import usersResponsesMessages from "../constant/response-messages/usersResponsesMessages.ts";
 
 export class UsersController {
     constructor(
@@ -17,5 +18,21 @@ export class UsersController {
         }
 
         return c.json({ error: authResponsesMessages.noUser }, 404)
+    }
+
+    async updateUserData(c: Context<{ Variables: { userEmail: string } }, never, { out: { form: UpdateUserData } }>) {
+        const updateData = c.req.valid("form");
+        const userEmail = c.get("userEmail");
+
+        if (!Object.keys(updateData).length) {
+            return c.json({ error: usersResponsesMessages.noUpdateData }, 400)
+        }
+
+        const result = await this.usersService.update(userEmail, updateData);
+        if (result) {
+            return c.json({ result: usersResponsesMessages.successfulUpdate })
+        }
+
+        return c.json({ error: usersResponsesMessages.failedUpdate }, 400)
     }
 }
