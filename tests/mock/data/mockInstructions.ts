853504af7a5cf37068f71f21d5ca1f11631c9045
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-03 01:13:23 +0300

    Create a file of mock instructions for testing

---
+++ tests/mock/data/mockInstructions.ts
@@ -0,0 +1,32 @@
+
+export const mockInstructions: Instruction[] = [
+    {
+        id: "66b6426ee5e71a872c338770",
+        datasetId: "66b63caab731d0a4ba127d70",
+        prompt: "How old is Abdulrhman Goni?",
+        response: "Abdulrhman Goni is 22",
+        userEmail: "example1@gmail.com"
+    },
+    {
+        id: "66b6426ee5e71a872c338771",
+        datasetId: "66b63caab731d0a4ba127d70",
+        systemMessage: "Response in one word",
+        prompt: "What is the capital of Nigeria?",
+        response: "The capital of Nigeria is Abuja",
+        userEmail: "example2@gmail.com"
+    },
+    {
+        id: "66b6426ee5e71a872c338772",
+        datasetId: "66b63caab731d0a4ba127d70",
+        systemMessage: "Response in one word",
+        prompt: "What is the job title of Abdulrhman Goni?",
+        response: "The job title of Abdulrhman Goni is 'Software Engineer'?",
+        userEmail: "example2@gmail.com"
+    }
+]
+
+export function getRandomMockInstruction() {
+    return mockInstructions[Math.floor(mockInstructions.length * Math.random())]
+}
+
+export default mockInstructions;
