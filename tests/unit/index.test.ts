98113bb89cfc55d23292e55269656ac1d4edf25f
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-03 01:10:37 +0300

    Write unit tests for `objectIdToUUID` helper function
    First create the entrypoint of all unit tests and then create
    the tests file of `objectIdToUUID` helper function.

---
+++ tests/unit/index.test.ts
@@ -0,0 +1,8 @@
+import { describe } from "@std/testing/bdd";
+import objectIdToUUID_test from "./objectIdToUUID.test.ts";
+
+describe("Unit tests", () => {
+    describe("Testing `objectIdToUUID` helper function", () => {
+        objectIdToUUID_test()
+    })
+})
