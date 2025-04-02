39a10e74ff558696671972b69663917b4dbfc219
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-03 01:15:58 +0300

    Write a unit test for `readFileLineByLine` function

--- tests/unit/index.test.ts
+++ tests/unit/index.test.ts
@@ -1,6 +1,7 @@
 import { describe } from "@std/testing/bdd";
 import objectIdToUUID_test from "./objectIdToUUID.test.ts";
 import promptTemplete_test from "./promptTemplete.test.ts";
+import readFileLineByLine_test from "./readFileLineByLine.test.ts";
 
 describe("Unit tests", () => {
     describe("Testing `objectIdToUUID` helper function", () => {
@@ -11,4 +12,7 @@ describe("Unit tests", () => {
         promptTemplete_test()
     })
 
+    describe("Testing `readFileLineByLine` helper function", () => {
+        readFileLineByLine_test()
+    })
 })
