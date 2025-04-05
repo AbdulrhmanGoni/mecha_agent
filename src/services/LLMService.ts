5a0ea456e9188e220d59397bbf5d9e8f523ca45f
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-06 01:55:13 +0300

    Refactor `LLMService` to use an instance of `LLMClientInterface`
    Make `LLMService` class use an instance of `LLMClientInterface`
    interface to interact with the LLM instead of using the LLM client
    directly.

--- src/services/LLMService.ts
+++ src/services/LLMService.ts
@@ -1,25 +1,7 @@
-import { Message, Ollama } from "ollama";
-import parsedEnvVariables from "../configurations/parseEnvironmentVariables.ts";
-
 export class LLMService {
-    constructor(private readonly ollamaClient: Ollama) { }
+    constructor(private readonly llmClient: LLMClientInterface) { }
 
-    async generate(prompt: string, system: string) {
-        return await this.ollamaClient.generate({
-            model: parsedEnvVariables.MODEL_NAME,
-            prompt,
-            system,
-            keep_alive: "1h",
-            stream: true,
-        })
-    }
-
-    async chat(messages: Message[]) {
-        return await this.ollamaClient.chat({
-            model: parsedEnvVariables.MODEL_NAME,
-            messages,
-            keep_alive: "1h",
-            stream: true,
-        })
+    async chat(messages: ChatMessage[], options?: ChatRequestOptions) {
+        return await this.llmClient.chat(messages, options)
     }
 }
