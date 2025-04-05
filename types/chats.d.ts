ad09b8104fa83bd4648eaa42897934ad6ebb639a
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-05 17:35:38 +0300

    Create `LLMClientInterface` & `EmbeddingClientInterface` interfaces
    The purpose of these interfaces is to abstract the LLM client and the
    embedding client from the services, so then to create more than client
     implement these interfaces and each implementation will be dedicated
     to a specefic environment.

--- types/chats.d.ts
+++ types/chats.d.ts
@@ -1,4 +1,20 @@
-type ChatMessage = Pick<import("ollama").Message, "content" | "role">;
+type ChatMessage = {
+    role: string;
+    content: string;
+}
+
+interface LLMClientInterface {
+    chat(messages: ChatMessage[], options?: ChatRequestOptions): Promise<ReadableStream>;
+}
+
+interface EmbeddingClientInterface {
+    embedText(text: string): Promise<number[]>;
+    embeddingDimensions: number
+}
+
+type ChatRequestOptions = {
+    onResponseComplete?: (responseText: string) => Promise<void>;
+}
 
 type ChatHistory = {
     id: string;
