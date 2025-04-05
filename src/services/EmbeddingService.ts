02337dec76ccfbebb955b390689cf5ae3c0dada3
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-06 01:54:39 +0300

    Refactor `EmbeddingService` to use an instance of `EmbeddingClientInterface`
    Make `EmbeddingService` class use an instance of `EmbeddingClientInterface`
    interface to embed the datasets instructions and users prompts instead of
    using the LLM client directly.

--- src/services/EmbeddingService.ts
+++ src/services/EmbeddingService.ts
@@ -1,17 +1,11 @@
-import { Ollama } from "ollama";
-import parsedEnvVariables from "../configurations/parseEnvironmentVariables.ts";
-
 export class EmbeddingService {
-    constructor(private readonly ollamaClient: Ollama) { }
+    constructor(private readonly embeddingClient: EmbeddingClientInterface) { }
+
+    public get embeddingDimensions() {
+        return this.embeddingClient.embeddingDimensions
+    }
 
     async embedText(text: string) {
-        return await this.ollamaClient.embeddings({
-            keep_alive: "1h",
-            model: parsedEnvVariables.EMBEDDING_MODEL_NAME,
-            prompt: text,
-            options: {
-                embedding_only: true,
-            }
-        });
+        return await this.embeddingClient.embedText(text);
     }
 }
