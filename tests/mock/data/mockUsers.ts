27f4bc638526acca4959464dcc8b6297ca3ed20d
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-05 17:27:23 +0300

    Create mock users for tests

---
+++ tests/mock/data/mockUsers.ts
@@ -0,0 +1,14 @@
+export const fakeUserEmail = "fakeemail@yahoo.com";
+
+export const testingUserCredentials = {
+    username: "Testing User",
+    email: "testinguser@gmail.com",
+    password: "fgkj6!fgeb4-strong-password-r347!@$27657",
+    signingMethod: "github",
+};
+
+export const returnedUserCredentials = {
+    email: "testinguser@gmail.com",
+    password: "fgkj6!fgeb4-strong-password-r347!@$27657",
+    signingMethod: "github",
+};
