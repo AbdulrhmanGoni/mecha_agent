632ff681df72d623021e5715594b078b7dbb7ce3
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-05 17:41:51 +0300

    Create mock API Keys for tests

---
+++ tests/mock/data/mockApiKeys.ts
@@ -0,0 +1,41 @@
+import { randomUUID } from "node:crypto";
+import randomString from "../../../src/helpers/randomString.ts";
+
+export const mockApiKeys: ApiKeyRecord[] = [
+    {
+        key: randomString(30),
+        keyName: "API Key 1",
+        id: randomUUID(),
+        permissions: ["inference", "read", "write"],
+        status: "Active",
+        userEmail: "useremail23456@gmail.com",
+        createdAt: new Date(),
+        expirationDate: new Date(),
+    },
+    {
+        key: randomString(30),
+        keyName: "API Key 2",
+        id: randomUUID(),
+        permissions: ["read", "write"],
+        status: "Active",
+        userEmail: "useremail23456@gmail.com",
+        createdAt: new Date(),
+        expirationDate: new Date(),
+    },
+    {
+        key: randomString(30),
+        keyName: "API Key 3",
+        id: randomUUID(),
+        permissions: ["inference"],
+        status: "Active",
+        userEmail: "useremail23456@gmail.com",
+        createdAt: new Date(),
+        expirationDate: new Date(),
+    },
+]
+
+export const newApiKeyInput: Omit<CreateApiKeyInput, "userEmail"> = {
+    keyName: "new api key",
+    maxAgeInDays: 8,
+    permissions: ["read", "write"],
+}
