610c6abc1124e2866f060a2c805d3270cef2a1f5
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-06 14:13:21 +0300

    Create the function that bootstraps the embedding client
    This function handles bootstrapping the embedding client based
    on the environment whether it's "production", "development" or
    "testing".

---
+++ src/configurations/bootstrapEmbeddingClient.ts
@@ -0,0 +1,42 @@
+import { Ollama as OllamaClient } from "ollama";
+import { GoogleGenAI } from "@google/genai";
+import parsedEnvVariables from "./parseEnvironmentVariables.ts";
+import { GoogleEmbeddingClient } from "./googleEmbeddingsClient.ts";
+import { MockEmbeddingClient } from "../../tests/mock/configs/mockEmbeddingClient.ts";
+import { OllamaEmbeddingClient } from "./ollamaEmbeddingsClient.ts";
+
+export function bootstrapEmbeddingClient() {
+    let embeddingClient: EmbeddingClientInterface;
+
+    switch (parsedEnvVariables.DENO_ENV) {
+        case "production": {
+            if (!parsedEnvVariables.GEMINI_API_KEY) {
+                throw new Error("'GEMINI_API_KEY' environment variable is missing");
+            }
+
+            const googleGenAI = new GoogleGenAI({ apiKey: parsedEnvVariables.GEMINI_API_KEY });
+            embeddingClient = new GoogleEmbeddingClient(googleGenAI);
+            break;
+        }
+
+        case "development": {
+            if (!parsedEnvVariables.GEMINI_API_KEY) {
+                throw new Error("'GEMINI_API_KEY' environment variable is missing");
+            }
+
+            const ollamaClient = new OllamaClient({ host: parsedEnvVariables.OLLAMA_HOST });
+            embeddingClient = new OllamaEmbeddingClient(ollamaClient);
+            break;
+        }
+
+        case "testing": {
+            embeddingClient = new MockEmbeddingClient();
+            break;
+        }
+
+        default:
+            throw new Error("'DENO_ENV' environment variable is neither 'testing', 'development' nor 'production'");
+    }
+
+    return embeddingClient
+};
