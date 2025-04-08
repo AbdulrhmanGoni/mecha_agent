2d70b8809d133adcaaca5d934f917984b2bd0583
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-08 23:43:03 +0300

    Create `insertAgentsIntoDB` helper function for tests
    Create this function to add an agent into the database for
    testing purpose.

---
+++ tests/helpers/insertAgentsIntoDB.ts
@@ -0,0 +1,30 @@
+import { Client as PostgresClient } from "deno.land/x/postgres";
+
+export default async function insertAgentsIntoDB(
+    { db, agents }:
+        { db: PostgresClient, agents: (Pick<Agent, "agentName" | "description" | "userEmail"> & { id?: string })[] }
+) {
+    const withId = !!agents[0].id
+    const fields = `agent_name, description, user_email${withId ? ", id" : ""}`;
+    const fieldsCount = fields.split(",").length
+
+    const placeholders = agents.map((_, i) => (
+        `($${i * fieldsCount + 1}, $${i * fieldsCount + 2}, $${i * fieldsCount + 3}${withId ? `,$${i * fieldsCount + 4}` : ""})`
+    )).join(", ")
+
+    const valuse = agents.flatMap((agent) => {
+        const vals = [agent.agentName, agent.description, agent.userEmail]
+        if (withId) {
+            vals.push(agent.id || "")
+        }
+        return vals
+    })
+
+    const { rows } = await db.queryObject<{ id: string }>({
+        text: `INSERT INTO agents(${fields}) VALUES ${placeholders} RETURNING id;`,
+        args: valuse
+    })
+
+    return rows
+};
+
