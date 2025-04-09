bdf34c2c3f441f9915f3d4165c9aaee938b462be
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-09 13:30:13 +0300

    Create `createTestingDatasetForAgent` helper function for tests
    Create this function to make a testing dataset for an agent for testing
    purpose.

---
+++ tests/helpers/createTestingDatasetForAgent.ts
@@ -0,0 +1,93 @@
+import { Client as PostgresClient } from "deno.land/x/postgres";
+import { QdrantClient } from "npm:@qdrant/js-client-rest";
+// @deno-types="minio/dist/esm/minio.d.mts"
+import { Client as MinioClient } from "minio";
+import insertUserIntoDB from "./insertUserIntoDB.ts";
+import insertAgentsIntoDB from "./insertAgentsIntoDB.ts";
+import mockInstructions from "../mock/data/mockInstructions.ts";
+import { getMockDatasetFile, mockDatasetForAgents } from "../mock/data/mockDataset.ts";
+import { Readable } from "node:stream";
+
+type Params = {
+    db: PostgresClient,
+    vectorDB?: QdrantClient,
+    objectStorage: MinioClient,
+    user: SignUpUserInput,
+    agent: (Pick<Agent, "agentName" | "description"> & { id?: string }),
+    dontEmbed?: boolean,
+}
+
+export default async function createTestingDatasetForAgent({ db, vectorDB, objectStorage, user, agent, dontEmbed }: Params) {
+    await insertUserIntoDB({ db, user })
+    const [{ id: newAgentId }] = await insertAgentsIntoDB({
+        db,
+        agents: [{
+            agentName: agent.agentName,
+            description: agent.description,
+            userEmail: user.email,
+            id: agent.id
+        }]
+    })
+
+    const newDatasetId = crypto.randomUUID();
+
+    const newDataset = mockDatasetForAgents({
+        id: newAgentId,
+        agentName: agent.agentName,
+    })
+
+    const { rows: [newDatasetData] } = await db.queryObject<Dataset>({
+        text: `
+            INSERT INTO datasets (id, title, description, agent_id, status, user_email) 
+            VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING *;
+        `,
+        args: [newDatasetId, newDataset.title, newDataset.description, newAgentId, "processed", user.email],
+        camelCase: true
+    })
+
+    await db.queryObject(
+        `UPDATE agents SET dataset_id = $1 WHERE id = $2`,
+        [newDatasetId, newAgentId]
+    )
+
+    const mockDatasetFile = getMockDatasetFile()
+
+    await objectStorage.putObject(
+        "datasets",
+        newDatasetId,
+        Readable.from(mockDatasetFile.stream()),
+        mockDatasetFile.size,
+        { "user-email": user.email, 'Content-Type': mockDatasetFile.type },
+    )
+
+    if (!dontEmbed && vectorDB) {
+        const testingEmbeddingFileURL = import.meta.resolve("../mock/data/testingEmbedding.json");
+        const testingEmbedding = JSON.parse(Deno.readTextFileSync(testingEmbeddingFileURL.replace("file://", "")));
+
+        await vectorDB.upsert(
+            "datasets",
+            {
+                wait: true,
+                points: mockInstructions.map((instruction) => {
+                    return {
+                        payload: {
+                            ...instruction,
+                            datasetId: newDatasetId,
+                            userEmail: user.email,
+                            id: instruction.id,
+                        },
+                        id: crypto.randomUUID(),
+                        vector: testingEmbedding
+                    }
+                })
+            }
+        )
+    }
+
+    return {
+        newDatasetId,
+        newDatasetData,
+        newAgentId,
+    }
+};
