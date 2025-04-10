c509c973f8f58fd88a8e79b5910e53f1d5bef725
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-10 18:41:47 +0300

    Add three more e2e tests to `signUp.test.ts` file

--- tests/e2e/auth/signUp.test.ts
+++ tests/e2e/auth/signUp.test.ts
@@ -1,15 +1,17 @@
 import { afterAll, describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
 import { MechaTester } from "../../helpers/mechaTester.ts";
+import type { JwtService } from "../../../src/services/JwtService.ts";
 import { Client as PostgresClient } from "deno.land/x/postgres";
 import { testingUserCredentials } from "../../mock/data/mockUsers.ts";
+import authResponsesMessages from "../../../src/constant/response-messages/authResponsesMessages.ts";
 
 export default function signUpTests({ db }: { db: PostgresClient }) {
     const endpoint = "/api/auth/sign-up";
 
     describe(`Testing 'POST ${endpoint}' route`, () => {
         afterAll(async () => {
-            await db.queryObject`DELETE FROM users WHERE 1 = 1`;
+            await db.queryObject`DELETE FROM users`;
         })
 
         it("Should return validation error because of the a short password", async () => {
@@ -65,6 +67,53 @@ export default function signUpTests({ db }: { db: PostgresClient }) {
 
             expect(res.error).toMatch(/signingMethod/g);
             expect(res.error).toMatch(/Required/g);
+        });
+
+        it("Should succeed to sign up because of the correct cridentials", async () => {
+            const request = new MechaTester(testingUserCredentials.email);
+
+            const response = await request.post(endpoint)
+                .json(testingUserCredentials)
+                .headers({ "Content-Type": "application/json" })
+                .send()
+
+            const res = await response.json<{ result: ReturnType<JwtService["generateJwt"]> }>()
+
+            expect(res.result).toMatchObject({
+                name: testingUserCredentials.username,
+                email: testingUserCredentials.email,
+                avatar: null,
+            });
+        });
+
+        it("Should fail to sign up because the user is already existing", async () => {
+            const request = new MechaTester(testingUserCredentials.email);
+
+            const response = await request.post(endpoint)
+                .json(testingUserCredentials)
+                .headers({ "Content-Type": "application/json" })
+                .send()
+
+            const res = await response.json<{ error: string }>()
+
+            expect(res.error).toBe(authResponsesMessages.userAlreadyExisting);
+        });
+
+        it("Should succeed sign in because the user is already existing and using 'signing-existing-user' query parameter", async () => {
+            const request = new MechaTester(testingUserCredentials.email);
+
+            const response = await request.post(endpoint + "?signing-existing-user=yes")
+                .json(testingUserCredentials)
+                .headers({ "Content-Type": "application/json" })
+                .send()
+
+            const res = await response.json<{ result: object }>()
+
+            expect(res.result).toMatchObject({
+                name: testingUserCredentials.username,
+                email: testingUserCredentials.email,
+                avatar: null,
+            });
         });
     });
 };
