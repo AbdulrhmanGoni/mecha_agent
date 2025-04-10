1ce7e3a996884ee9c94d01b7d1aa481fdd4dbeea
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-11 02:11:20 +0300

    Update the value of `chatsResponsesMessages.dontKnow`

--- src/constant/response-messages/chatsResponsesMessages.ts
+++ src/constant/response-messages/chatsResponsesMessages.ts
@@ -1,6 +1,6 @@
 const chatsResponsesMessages = {
     noDataset: "Sorry!, I don't have a dataset to answer based on.",
-    dontKnow: "Sorry!, I don't have enough information to answer",
+    dontKnow: "Sorry!, I don't have enough information to answer you properly",
 }
 
 export default chatsResponsesMessages
