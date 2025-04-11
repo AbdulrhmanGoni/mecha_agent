ddb67a4b1828711fcaf600ded6a0a57a8d069bc9
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-12 00:22:20 +0300

    Create the entrypoint of all ene to end (e2e) tests

---
+++ tests/e2e/index.test.ts
@@ -0,0 +1,12 @@
+import { describe } from "@std/testing/bdd";
+import bootstrapTestingConfigs from "../bootstrapTestingConfigs.ts";
+import authRouteTests from "./auth/index.test.ts";
+import usersRouteTests from "./users/index.test.ts";
+
+const app = await bootstrapTestingConfigs()
+
+describe("Ene to end (e2e) tests", () => {
+    authRouteTests(app);
+
+    usersRouteTests(app);
+})
