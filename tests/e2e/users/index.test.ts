d23144cadafa63dacf1edd487dd1988eaa80e85d
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-12 00:20:35 +0300

    Create the file that groups users route e2e tests

---
+++ tests/e2e/users/index.test.ts
@@ -0,0 +1,17 @@
+import { describe } from "@std/testing/bdd";
+import getUserDataTests from "./getUserData.test.ts";
+import updateUserDataTests from "./updateUserData.test.ts";
+
+export default function usersRouteTests(app: TestingAppConfigs) {
+    describe("Testing users API route", () => {
+        getUserDataTests({
+            db: app.configurations.databaseClient
+        });
+
+        updateUserDataTests({
+            db: app.configurations.databaseClient,
+            objectStorage: app.configurations.minioClient
+        });
+    })
+};
+
