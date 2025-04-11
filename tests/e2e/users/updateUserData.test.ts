5b9d5cdf37d9abd3dbba7da1ceb859c77c47b6ea
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-11 14:34:20 +0300

    Add two test cases to `updateUserData.test.ts` file

--- tests/e2e/users/updateUserData.test.ts
+++ tests/e2e/users/updateUserData.test.ts
@@ -7,6 +7,7 @@ import insertUserIntoDB from "../../helpers/insertUserIntoDB.ts";
 import usersResponsesMessages from "../../../src/constant/response-messages/usersResponsesMessages.ts";
 // @deno-types="minio/dist/esm/minio.d.mts"
 import { Client as MinioClient } from "minio";
+import { uuidMatcher } from "../../helpers/uuidMatcher.ts";
 
 export default function updateUserDataTests({ db, objectStorage }: { db: PostgresClient, objectStorage: MinioClient }) {
     const endpoint = "/api/users";
@@ -38,6 +39,58 @@ export default function updateUserDataTests({ db, objectStorage }: { db: Postgre
             const res = await response.json<{ error: string }>()
 
             expect(res.error).toBe(usersResponsesMessages.noUpdateData);
+        });
+
+        it("Should succeed to update the name of the user", async () => {
+            const request = new MechaTester(testingUserCredentials.email);
+
+            const updateFormData = new FormData();
+            updateFormData.set("username", "New usaername");
+
+            const response = await request.patch(endpoint)
+                .body(updateFormData)
+                .send()
+
+            const { result } = await response.json<{ result: string }>()
+
+            expect(result).toBe(usersResponsesMessages.successfulUpdate);
+
+            const { rows: [updatedUser] } = await db.queryObject<{ username: string }>(
+                `SELECT username FROM users WHERE email = $1`,
+                [testingUserCredentials.email]
+            )
+
+            expect(updatedUser.username).toBe(updateFormData.get("username"));
+        });
+
+        it("Should succeed to update user's avatar", async () => {
+            const request = new MechaTester(testingUserCredentials.email);
+
+            const avatarFileURL = import.meta.resolve("../../mock/media/fake-avatar.png")
+            const avatarFile = new Blob(
+                [Deno.readFileSync(avatarFileURL.replace("file://", ""))],
+                { type: "image/png" }
+            );
+
+            const updateFormData = new FormData();
+            updateFormData.set("newAvatar", avatarFile);
+
+            const response = await request.patch(endpoint)
+                .body(updateFormData)
+                .send()
+
+            const { result } = await response.json<{ result: string }>()
+
+            expect(result).toBe(usersResponsesMessages.successfulUpdate);
+
+            const { rows: [updatedUser] } = await db.queryObject<{ avatar: string }>(
+                `SELECT avatar FROM users WHERE email = $1`,
+                [testingUserCredentials.email]
+            )
+
+            if (updatedUser.avatar) avatarsToDelete.push(updatedUser.avatar);
+
+            expect(updatedUser.avatar.split(".")[0]).toMatch(uuidMatcher);
         });
     });
 };
