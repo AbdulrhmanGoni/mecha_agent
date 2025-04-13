59ecfc4a9b82f7e2f26c076eabb4f5b76200cf25
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-13 15:27:41 +0300

    Create the e2e tests file of `GET /api/api-keys` endpoint
    Create `tests/e2e/agents/getAllApiKeys.test.ts` file and write
    inside it three e2e test cases for `GET /api/api-keys` endpoint.

---
+++ tests/e2e/apiKeys/getAllApiKeys.test.ts
@@ -0,0 +1,63 @@
+import { afterAll, describe, it } from "@std/testing/bdd";
+import { expect } from "@std/expect";
+import { MechaTester } from "../../helpers/mechaTester.ts";
+import { Client as PostgresClient } from "deno.land/x/postgres";
+import { fakeUserEmail, testingUserCredentials } from "../../mock/data/mockUsers.ts";
+import insertUserIntoDB from "../../helpers/insertUserIntoDB.ts";
+import { newApiKeyInput } from "../../mock/data/mockApiKeys.ts";
+import { uuidMatcher } from "../../helpers/uuidMatcher.ts";
+import insertApiKeysIntoDB from "../../helpers/insertApiKeysIntoDB.ts";
+
+export default function getAllApiKeysTests({ db }: { db: PostgresClient }) {
+    const endpoint = "/api/api-keys";
+
+    describe(`Testing 'GET ${endpoint}' route`, () => {
+        afterAll(async () => {
+            await db.queryObject`
+                DELETE FROM api_keys; 
+                DELETE FROM users;
+            `;
+        });
+
+        it("Should fail to get any keys because the user doesn't exist", async () => {
+            const request = new MechaTester(fakeUserEmail);
+            const response = await request.get(endpoint).send()
+            const res = await response.json<{ result: [] }>();
+
+            expect(res.result).toBeInstanceOf(Array);
+            expect(res.result).toHaveLength(0);
+        });
+
+        it("Should get an empty array because the user doesn't have any api keys", async () => {
+            await insertUserIntoDB({ db, user: testingUserCredentials })
+
+            const request = new MechaTester(testingUserCredentials.email);
+            const response = await request.get(endpoint).send()
+            const res = await response.json<{ result: [] }>();
+
+            expect(res.result).toBeInstanceOf(Array);
+            expect(res.result).toHaveLength(0);
+        });
+
+        it("Should get an array of user's api keys", async () => {
+            await insertApiKeysIntoDB({ db, keys: [{ ...newApiKeyInput, userEmail: testingUserCredentials.email }] });
+
+            const request = new MechaTester(testingUserCredentials.email);
+            const response = await request.get(endpoint).send()
+            const res = await response.json<{ result: ApiKeyRecord[] }>();
+
+            expect(res.result).toBeInstanceOf(Array);
+            expect(res.result).toHaveLength(1);
+            expect(res.result[0]).toMatchObject({
+                id: expect.stringMatching(uuidMatcher),
+                key: expect.any(String),
+                keyName: newApiKeyInput.keyName,
+                expirationDate: expect.any(String),
+                permissions: expect.arrayContaining(newApiKeyInput.permissions),
+                status: "Active",
+                createdAt: expect.any(String),
+                userEmail: testingUserCredentials.email
+            });
+        });
+    });
+};
