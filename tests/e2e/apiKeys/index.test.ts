4c92103e4311ed21453cd625f838c4fa34d83ded
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-13 18:32:19 +0300

    Update the file that groups all api keys route tests
    Import `deactivateApiKeyTests` and `activateApiKeyTests`
    files into the file that groups all api keys route tests.

--- tests/e2e/apiKeys/index.test.ts
+++ tests/e2e/apiKeys/index.test.ts
@@ -2,6 +2,8 @@ import { describe } from "@std/testing/bdd";
 import createApiKeyTests from "./createApiKey.test.ts";
 import getAllApiKeysTests from "./getAllApiKeys.test.ts";
 import deleteApiKeysTests from "./deleteApiKeys.test.ts";
+import deactivateApiKeyTests from "./deactivateApiKeys.test.ts";
+import activateApiKeyTests from "./activateApiKeys.test.ts";
 
 export default function apiKeysRouteTests(app: TestingAppConfigs) {
     describe("Testing api-keys API route", () => {
@@ -14,6 +16,14 @@ export default function apiKeysRouteTests(app: TestingAppConfigs) {
         });
 
         deleteApiKeysTests({
+            db: app.configurations.databaseClient
+        });
+
+        deactivateApiKeyTests({
+            db: app.configurations.databaseClient
+        });
+
+        activateApiKeyTests({
             db: app.configurations.databaseClient
         });
     })
