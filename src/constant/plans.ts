f34645b4ba18ad0046c04b604d46cf4153f799c8
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-21 00:43:18 +0300

    Create `src/constant/plans.ts` file to put the plans of the app in

---
+++ src/constant/plans.ts
@@ -0,0 +1,16 @@
+export const plans: Plan[] = [
+    {
+        planName: "Free",
+        maxAgentsCount: 1,
+        maxApiKeysCount: 1,
+        maxInferencesCount: 30,
+        subscriptionCostPerMonth: 0,
+    },
+    {
+        planName: "Pro",
+        maxAgentsCount: 5,
+        maxApiKeysCount: 5,
+        maxInferencesCount: 240,
+        subscriptionCostPerMonth: 5,
+    },
+] 
