345f8fbe522578b8577c12e7cc100129da659671
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-04-23 23:57:40 +0300

    Delete some deprecated files that are no longer used

--- src/constant/root.ts
+++
@@ -1,2 +0,0 @@
-export const defaultRootUsername = "root"
-export const defaultRootPassword = "root-password"
--- src/validation/auth/rootUserSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-const rootUserSchema = z.object({
-    username: z.string().min(3).max(70),
-    password: z.string().min(6).max(200),
-})
-
-export default rootUserSchema
