e913d35114e933d55a7a4cc293108c4c97d748b2
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-05-04 01:42:22 +0300

    Add `--unstable-kv` flag to "run tests" command
    Add this flag to "run tests" command in `tests/scripts/runTests.ts`
    file in order to Deno KV Database be able to work in testing environment.

--- tests/scripts/runTests.ts
+++ tests/scripts/runTests.ts
@@ -2,7 +2,7 @@ import process from "node:process";
 
 const testType = process.argv[process.argv.length - 1];
 
-const testCommand = `exec testing_server_container deno test -N -R -E --env-file=docker/.env.testing tests/${testType}/index.test.ts`
+const testCommand = `exec testing_server_container deno test -N -R -E --unstable-kv --env-file=docker/.env.testing tests/${testType}/index.test.ts`
 
 const runTestsCommand = new Deno.Command("docker", {
     args: testCommand.split(" ")
