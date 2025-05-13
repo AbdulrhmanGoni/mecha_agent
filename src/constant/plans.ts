516d00b3f8812700b2dcc3c62e7d10e626c57a84
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-05-13 14:37:11 +0300

    Update plans

--- src/constant/plans.ts
+++ src/constant/plans.ts
@@ -1,3 +1,5 @@
+import parsedEnvVariables from "../configurations/parseEnvironmentVariables.ts";
+
 export const plans: Plan[] = [
     {
         planName: "Free",
@@ -5,12 +7,14 @@ export const plans: Plan[] = [
         maxApiKeysCount: 1,
         maxInferencesPerDay: 30,
         subscriptionCostPerMonth: 0,
+        priceId: ""
     },
     {
         planName: "Pro",
         maxAgentsCount: 5,
         maxApiKeysCount: 5,
         maxInferencesPerDay: 240,
-        subscriptionCostPerMonth: 5,
+        subscriptionCostPerMonth: 4.99,
+        priceId: parsedEnvVariables.PRO_SUBSCRIPTION_PRICE_ID as string
     },
 ] 
