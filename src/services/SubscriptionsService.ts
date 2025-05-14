7247e24a964721c29c60fa131539f7fdc282baa9
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-05-14 13:30:51 +0300

    Create `SubscriptionsService` class with three methods

---
+++ src/services/SubscriptionsService.ts
@@ -0,0 +1,68 @@
+import { DatabaseService } from "./DatabaseService.ts";
+
+export class SubscriptionsService {
+    constructor(
+        private paymentGatewayClientInterface: PaymentGatewayClientInterface,
+        private databaseService: DatabaseService,
+        private kvStoreClient: Deno.Kv,
+    ) { }
+
+    async createSubscriptionSession(userEmail: string, plan: Plan) {
+        const { rows: [user] } = await this.databaseService.query<Pick<User, "currentPlan">>({
+            text: "SELECT current_plan FROM users WHERE email = $1",
+            args: [userEmail],
+            camelCase: true,
+        });
+
+        if (user.currentPlan === plan.planName) {
+            return {
+                success: false,
+                samePlan: true,
+            }
+        }
+
+        const session = await this.paymentGatewayClientInterface.createSubscriptionSession(
+            userEmail,
+            plan.priceId
+        )
+
+        return {
+            success: true,
+            session,
+        }
+    }
+
+    async verifyCheckoutSessionExistence(sessionId: string) {
+        return await this.paymentGatewayClientInterface.verifyCheckoutSessionExistence(sessionId);
+    }
+
+    async createSubscription(userEmail: string, customerId: string, subscriptionId: string) {
+        const session = this.databaseService.createTransaction("subscription_creation");
+        await session.begin();
+
+        const { rowCount } = await session.queryObject<{ id: string }>({
+            text: `
+                INSERT INTO subscriptions(customer_id, subscription_id, user_email, plan) 
+                VALUES($1, $2, $3, $4)
+            `,
+            args: [customerId, subscriptionId, userEmail, "Pro"],
+        });
+
+        if (rowCount) {
+            const { rowCount } = await session.queryObject({
+                text: 'UPDATE users SET subscription_id = $2, current_plan = $3 WHERE email = $1',
+                args: [userEmail, subscriptionId, "Pro"],
+            });
+
+            if (rowCount) {
+                await session.commit()
+                await this.kvStoreClient.delete(["inferences", userEmail, "Free"]);
+
+                return true
+            }
+        }
+
+        await session.rollback()
+        return false
+    }
+}
