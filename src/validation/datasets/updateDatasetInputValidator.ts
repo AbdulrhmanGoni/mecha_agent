7da1b0a3f6d4fe4dbf809c3aa00bdda85b816e07
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-11 00:04:23 +0300

    Update the schema of the dataset validator
    Remove `agentId` and `datasetFile` fields from  the schema
    of the dataset validator.

--- src/validation/datasets/updateDatasetInputValidator.ts
+++ src/validation/datasets/updateDatasetInputValidator.ts
@@ -2,14 +2,15 @@ import { z } from 'zod'
 import { validator } from 'hono/validator'
 import schemaParser from "../../helpers/schemaParser.ts";
 import datasetSchema from "./datasetSchema.ts";
+import { ParsedFormValue } from "hono/types";
 
 export const updateDatasetInputSchema = z.object({
     title: datasetSchema.shape.title.optional(),
     description: datasetSchema.shape.description.optional(),
 }).strict();
 
-const updateDatasetInputValidator = validator('json', (value, c) => {
-    return schemaParser<typeof updateDatasetInputSchema.shape>(
+const updateDatasetInputValidator = validator('form', (value, c) => {
+    return schemaParser<typeof updateDatasetInputSchema.shape, ParsedFormValue | ParsedFormValue[]>(
         c,
         updateDatasetInputSchema,
         value
