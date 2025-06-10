d92f4a16fab5addfc3861700e15cf2c782a7ef08
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-10 23:28:03 +0300

    Cancel uploading and processing a dataset file when creating a new dataset
    Cancel the process of uploading and processing a dataset file when creating
    a new dataset for the first time. instead, dataset files should be uploaded
    later after creating an empty dataset record.

--- src/services/DatasetsService.ts
+++ src/services/DatasetsService.ts
@@ -9,91 +9,20 @@ export class DatasetsService {
         private readonly datasetProcessingWorker: Worker,
         private readonly sseService: SSEService,
     ) {
-        this.datasetProcessingWorker.addEventListener("message", async (e) => {
-            switch (e.data.process) {
-                case "successful_process":
-                    await this.update(
-                        e.data.payload.datasetId,
-                        e.data.payload.userEmail,
-                        { status: "processed" }
-                    );
-                    this.sseService.dispatchEvent({
-                        event: "dataset-status",
-                        target: e.data.payload.datasetId,
-                        message: "processed"
-                    });
-                    break;
-                case "failed_process":
-                    await this.update(
-                        e.data.payload.datasetId,
-                        e.data.payload.userEmail,
-                        { status: "unprocessed" }
-                    );
-                    this.sseService.dispatchEvent({
-                        event: "dataset-status",
-                        target: e.data.payload.datasetId,
-                        message: "unprocessed"
-                    });
-                    break;
-            }
-        })
-
-        this.datasetProcessingWorker.addEventListener("error", (e) => {
-            console.log("Error from 'datasetProcessingWorker' worker ! =>", e.error)
-        })
     }
 
     async create(userEmail: string, datasetInput: CreateDatasetInput) {
-        const newDatasetId = crypto.randomUUID();
-
-        const transactionSession = this.databaseService.createTransaction("dataset_creation");
-        await transactionSession.begin();
-
-        const { rows: [dataset] } = await transactionSession.queryObject<Dataset>({
-            text: `
-                INSERT INTO datasets (id, agent_id, title, description, status, user_email) 
-                VALUES ($1, $2, $3, $4, $5, $6)
-                RETURNING *;
-            `,
+        const { rows } = await this.databaseService.query<Dataset>({
+            text: 'INSERT INTO datasets (title, description, user_email) VALUES ($1, $2, $3) RETURNING *',
             args: [
-                newDatasetId,
-                datasetInput.agentId,
                 datasetInput.title,
                 datasetInput.description,
-                "processing",
                 userEmail
             ],
             camelCase: true
         });
 
-        const { rowCount: updatedAgents } = await transactionSession.queryObject<Dataset>({
-            text: "UPDATE agents SET dataset_id = $2 WHERE id = $1 AND user_email = $3",
-            args: [dataset.agentId, dataset.id, dataset.userEmail],
-        });
-
-        if (dataset && updatedAgents === 1) {
-            try {
-                await this.objectStorageService.uploadFile(
-                    this.objectStorageService.buckets.datasets,
-                    datasetInput.datasetFile,
-                    { id: dataset.id, metaData: { "user-email": userEmail } }
-                );
-
-                await transactionSession.commit();
-
-                this.datasetProcessingWorker.postMessage({
-                    process: "new_dataset",
-                    payload: dataset,
-                });
-
-                return dataset;
-            } catch {
-                //
-            }
-        }
-
-        await transactionSession.rollback();
-        return null;
+        return rows[0]
     }
 
     async getOne(datasetId: string, userEmail: string) {
