5049a04061081a0d0416d84ba377c6c649c61a45
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-14 14:28:12 +0300

    Update `embedInstructionFormat` helper function
    Update its parameter's type and remove `systemMessage` field access
    from it.

--- src/helpers/embedInstructionFormat.ts
+++ src/helpers/embedInstructionFormat.ts
@@ -1,6 +1,5 @@
-export default function embedInstructionFormat(instruction: Omit<BaseInstruction | UpdateInstructionInput, "id" | "datasetId">) {
+export default function embedInstructionFormat(instruction: Pick<Instruction, "prompt" | "response">) {
     const instructionText = (
-        `${instruction.systemMessage ? `"${instruction.systemMessage}"` : ""}` +
         `"${instruction.prompt}" ` +
         `"${instruction.response}"`
     )
