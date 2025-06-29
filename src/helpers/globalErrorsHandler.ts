a4f24d0f7f4ec3a0c92d8a8da9b5185e24fae23c
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-06-29 22:01:13 +0300

    Add `Sentry` for monitoring and errors tracking

--- src/helpers/globalErrorsHandler.ts
+++ src/helpers/globalErrorsHandler.ts
@@ -2,11 +2,22 @@ import { HTTPResponseError } from "hono/types";
 import { Context } from "hono";
 import { HTTPException } from "hono/http-exception";
 import parsedEnvVariables from "../configurations/parseEnvironmentVariables.ts";
+import * as Sentry from "sentry";
 
-export default async (err: Error | HTTPResponseError, c: Context) => {
+export default async (err: Error | HTTPResponseError, c: Context<never, never, never>) => {
     if (parsedEnvVariables.ENVIRONMENT !== "production") {
         console.log(err)
     }
+
+    Promise.resolve().then(() => Sentry.captureException(err, {
+        user: { email: c.get("userEmail") },
+        extra: {
+            route: c.req.routePath,
+            fullUrl: c.req.url,
+            origin: "server",
+            payload: c.req.valid("json") || c.req.valid("form") || c.req.valid("query")
+        }
+    }))
 
     if (err instanceof HTTPException) {
         if (err.message) {
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
+import * as Sentry from "sentry";
 import bootstrapApp from "./app.ts";
 import parsedEnvVariables from "./configurations/parseEnvironmentVariables.ts";
+
+Sentry.init({
+    dsn: parsedEnvVariables.SENTRY_DSN,
+});
 
 const app = await bootstrapApp();
 
