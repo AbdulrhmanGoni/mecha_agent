c0f8a9e42523f08cd927d32bc926e8c26783990c
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-07-09 22:42:23 +0300

    Fix "zod schema TypeError"

--- src/configurations/parseEnvironmentVariables.ts
+++ src/configurations/parseEnvironmentVariables.ts
@@ -4,7 +4,7 @@ function parseEnvironmentVariables<
     T extends z.ZodObject<z.ZodRawShape, z.UnknownKeysParam, z.ZodTypeAny>,
 >(schema: T): z.infer<typeof schema> {
     const vars: Record<string, string | undefined> = {};
-    for (const key in schema._def.shape()) {
+    for (const key in schema.shape) {
         vars[key] = Deno.env.get(key);
     }
 
