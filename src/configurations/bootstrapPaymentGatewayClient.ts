1fadbc452e833be7aac89dd9d7a0844fb94f80fd
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-10-30 01:30:27 +0300

    Stop stripe initialization because subscriptions service is disabled

--- src/configurations/bootstrapPaymentGatewayClient.ts
+++ src/configurations/bootstrapPaymentGatewayClient.ts
@@ -1,19 +1,24 @@
 import { MockPaymentGatewayClient } from "../../tests/mock/configs/MockPaymentGatewayClient.ts";
 import { kvStoreClient } from "./denoKvStoreClient.ts";
-import parsedEnvVariables from "./parseEnvironmentVariables.ts";
-import { StripePaymentGatewayClient } from "./stripePaymentGatewayClient.ts";
+// import parsedEnvVariables from "./parseEnvironmentVariables.ts";
+// import { StripePaymentGatewayClient } from "./stripePaymentGatewayClient.ts";
 import Stripe from "stripe";
 
+/**
+ * Disabled temporarily
+ */
 export function bootstrapPaymentGatewayClient() {
-    if (parsedEnvVariables.ENVIRONMENT == "testing") {
-        return new MockPaymentGatewayClient({} as Stripe, kvStoreClient);
-    } else {
-        if (!parsedEnvVariables.STRIPE_SECRET_KEY) {
-            throw new Error("'STRIPE_SECRET_KEY' environment variable is missing");
-        }
-        const stripe = new Stripe(parsedEnvVariables.STRIPE_SECRET_KEY, {
-            apiVersion: "2025-03-31.basil",
-        });
-        return new StripePaymentGatewayClient(stripe, kvStoreClient);
-    }
+    return new MockPaymentGatewayClient({} as Stripe, kvStoreClient);
+
+    // if (parsedEnvVariables.ENVIRONMENT == "testing") {
+    //     return new MockPaymentGatewayClient({} as Stripe, kvStoreClient);
+    // } else {
+    //     if (!parsedEnvVariables.STRIPE_SECRET_KEY) {
+    //         throw new Error("'STRIPE_SECRET_KEY' environment variable is missing");
+    //     }
+    //     const stripe = new Stripe(parsedEnvVariables.STRIPE_SECRET_KEY, {
+    //         apiVersion: "2025-03-31.basil",
+    //     });
+    //     return new StripePaymentGatewayClient(stripe, kvStoreClient);
+    // }
 };
