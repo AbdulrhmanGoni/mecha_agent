d1e9aaadd65b4861b6e972c907c3e3612158b424
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-10-29 20:06:36 +0300

    Set up a bridge server for using "DenoKV GUI Client" app in development

--- src/configurations/denoKvStoreClient.ts
+++ src/configurations/denoKvStoreClient.ts
@@ -2,3 +2,8 @@ import parsedEnvVariables from "./parseEnvironmentVariables.ts";
 
 export const kvStoreClient = await Deno.openKv(parsedEnvVariables.ENVIRONMENT === "testing" ? ":memory:" : undefined)
 
+if (parsedEnvVariables.ENVIRONMENT === "development") {
+    // For more details, see: https://github.com/AbdulrhmanGoni/denokv-bridge-server
+    const { openBridgeServerInDeno } = await import("@denokv-gui-client/bridge-server")
+    openBridgeServerInDeno(kvStoreClient, 8989);
+}
