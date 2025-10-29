477b921030f8dc857da7357cc33d4447fcdda706
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-10-29 21:52:15 +0300

    Ignore `STRIPE_SECRET_KEY` env var in testing environment

--- src/configurations/bootstrapPaymentGatewayClient.ts
+++ src/configurations/bootstrapPaymentGatewayClient.ts
@@ -5,13 +5,12 @@ import { StripePaymentGatewayClient } from "./stripePaymentGatewayClient.ts";
 import Stripe from "stripe";
 
 export function bootstrapPaymentGatewayClient() {
-    if (!parsedEnvVariables.STRIPE_SECRET_KEY) {
-        throw new Error("'STRIPE_SECRET_KEY' environment variable is missing");
-    }
-
     if (parsedEnvVariables.ENVIRONMENT == "testing") {
         return new MockPaymentGatewayClient({} as Stripe, kvStoreClient);
     } else {
+        if (!parsedEnvVariables.STRIPE_SECRET_KEY) {
+            throw new Error("'STRIPE_SECRET_KEY' environment variable is missing");
+        }
         const stripe = new Stripe(parsedEnvVariables.STRIPE_SECRET_KEY, {
             apiVersion: "2025-03-31.basil",
         });
