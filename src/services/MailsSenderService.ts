2ca974e49c7327651759eda31b89e0f26fb9f511
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-11-13 01:02:56 +0300

    Ignore initiating `MailsSenderService`'s config in testing environment

--- src/services/MailsSenderService.ts
+++ src/services/MailsSenderService.ts
@@ -7,6 +7,22 @@ export class MailsSenderService {
     private transporter?: Transporter<SMTPTransport.SentMessageInfo, SMTPTransport.Options>
     private mailjet?: Mailjet.Client
     constructor() {
+        if (!parsedEnvVariables.MAIL_SENDER_USER || !parsedEnvVariables.MAIL_SENDER_PASS) {
+            if (parsedEnvVariables.ENVIRONMENT == "testing") {
+                return
+            }
+
+            let errorMessage = ""
+            if (!parsedEnvVariables.MAIL_SENDER_USER) {
+                errorMessage += "MAIL_SENDER_USER environment variable is no set\n"
+            }
+            if (!parsedEnvVariables.MAIL_SENDER_PASS) {
+                errorMessage += "MAIL_SENDER_PASS environment variable is no set"
+            }
+
+            throw new Error(errorMessage)
+        }
+
         const auth = {
             user: parsedEnvVariables.MAIL_SENDER_USER,
             pass: parsedEnvVariables.MAIL_SENDER_PASS,
@@ -17,7 +33,7 @@ export class MailsSenderService {
                 throw "'MAIL_SENDER_KEY' environment variable is missing"
             }
             this.mailjet = Mailjet.Client.apiConnect(parsedEnvVariables.MAIL_SENDER_KEY, auth.pass)
-        } else {
+        } else if (parsedEnvVariables.ENVIRONMENT == "development") {
             this.transporter = createTransport({
                 host: parsedEnvVariables.MAIL_SENDER_HOST,
                 port: Number(parsedEnvVariables.MAIL_SENDER_PORT),
@@ -50,7 +66,8 @@ export class MailsSenderService {
                 })
 
             return res.response.status == 200
-        } else {
+        }
+        else if (parsedEnvVariables.ENVIRONMENT == "development") {
             if (!this.transporter) {
                 throw new Error("Nodemailer's Transporter hasn't been created")
             }
@@ -62,5 +79,7 @@ export class MailsSenderService {
 
             return accepted.some(email => email === email);
         }
+
+        return false
     }
 }
