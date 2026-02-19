44a93270e9360aadce47c87d4d575f7e7609bfa2
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2026-02-19 22:56:41 +0300

    Update "@denokv-gui-client/bridge-server"'s page link

--- src/configurations/denoKvStoreClient.ts
+++ src/configurations/denoKvStoreClient.ts
@@ -3,7 +3,7 @@ import parsedEnvVariables from "./parseEnvironmentVariables.ts";
 export const kvStoreClient = await Deno.openKv(parsedEnvVariables.ENVIRONMENT === "testing" ? ":memory:" : undefined)
 
 if (parsedEnvVariables.ENVIRONMENT === "development") {
-    // For more details, see: https://github.com/AbdulrhmanGoni/denokv-bridge-server
+    // For more details, see: https://jsr.io/@denokv-gui-client/bridge-server
     const { openBridgeServerInDeno } = await import("@denokv-gui-client/bridge-server")
     openBridgeServerInDeno(kvStoreClient, { port: 8989, authToken: "@denokv-gui-client/bridge-server" });
 }
